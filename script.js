c4b42fb0978445a0a955e5a39ddd8782ab8146d5
Ryuzen Namikaze <44092338+rnamikaze@users.noreply.github.com>
2023-05-24 07:49:14 +0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,15 @@
+fetch('/data.json')
+  .then(response => response.json())
+  .then(data => {
+
+    var jsonData = data;
+    
+    for(let i = 0; i < jsonData.length; i++) {
+        document.getElementById(`icon_${i+1}`).setAttribute('src', jsonData[i].icon);
+        document.getElementById(`score_${i+1}`).innerText = jsonData[i].score;
+        document.getElementById(`category_${i+1}`).innerText = jsonData[i].category;
+    }
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
